2df1eff1dd77c2233e5be8efb02a4f2fccb31c80
suman <suman.giri007@gmail.com>
2024-11-06 16:20:55 +0530

    first commit

---
+++ src/main/java/com/automation/PlayWrightAutomation/PlayWrightAutomation/PlayWrightAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.automation.PlayWrightAutomation.PlayWrightAutomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayWrightAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayWrightAutomationApplication.class, args);
+	}
+
+}
